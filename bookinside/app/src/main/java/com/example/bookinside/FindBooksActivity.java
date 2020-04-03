ef6fa117b0da7e1185093fcde4b298c32ed0d2af
AlexandruPutanu <alexandruputanu>
2020-04-03 18:55:22 +0300

    Working "Find book"

--- bookinside/app/src/main/java/com/example/bookinside/FindBooksActivity.java
+++ bookinside/app/src/main/java/com/example/bookinside/FindBooksActivity.java
@@ -2,6 +2,7 @@ package com.example.bookinside;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,17 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -20,15 +32,64 @@ public class FindBooksActivity extends AppCompatActivity {
     SearchView mySearchView;
     ListView myList;
 
+    //    ////////////////////////
+    private static String SERVER = "http://192.168.1.7:3000";
+    RequestQueue queue;
+    JSONArray req;
+
+    public  void  GetBooks() {
+        //Define the endpoint called by funct
+        SERVER += "/get_books";
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+//            return point of POST
+        System.out.println("lookd");
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, SERVER, req, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                            books.add(new BookType(jsonObject.getString("titlu") + " - " + jsonObject.getString("autor"), R.drawable.star, R.drawable.patratel, R.drawable.go));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                myList.setAdapter(new MyAdapter(FindBooksActivity.this, R.layout.my_book_list, books));
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                System.out.println("Volley"+ error.toString());
+                progressDialog.dismiss();
+            }
+        });
+
+    //FOR LOCALHOST
+            jsonArrayRequest.setShouldCache(false);
+            jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(30000,
+                                                              DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                                                              DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            queue.add(jsonArrayRequest);
+}
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("HERE!!!");
         setContentView(R.layout.activity_find_books);
 
+        queue = Volley.newRequestQueue(this);
         btnBack = (ImageView) findViewById(R.id.iv_arrow_from_find_books);
         sectionBook = (TextView) findViewById(R.id.book_find_section_title);
         mySearchView = (SearchView) findViewById(R.id.search_book);
         myList = (ListView) findViewById(R.id.list_with_books);
+        req = new JSONArray();
 
         final String name = getIntent().getStringExtra("username");
         final String title = getIntent().getStringExtra("title");
@@ -46,11 +107,11 @@ public class FindBooksActivity extends AppCompatActivity {
         //2.List Item java class
         //3.Most Important - Custom Adapter extend ArrayAdapter class
 
-        for (int i = 0; i < book_list.length; i++) {
-            books.add(new BookType(book_list[i], R.drawable.star, R.drawable.patratel, R.drawable.go));
-        }
-
-        myList.setAdapter(new MyAdapter(FindBooksActivity.this, R.layout.my_book_list, books));
+//        for (int i = 0; i < book_list.length; i++) {
+//            books.add(new BookType(book_list[i], R.drawable.star, R.drawable.patratel, R.drawable.go));
+//        }
+//
+//        myList.setAdapter(new MyAdapter(FindBooksActivity.this, R.layout.my_book_list, books));
 
 //        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list){
 //
@@ -78,6 +139,15 @@ public class FindBooksActivity extends AppCompatActivity {
         mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
+                JSONObject obj = new JSONObject();
+                try{
+                    obj.put("title",query);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                req.put(obj);
+                GetBooks();
                 return false;
             }
 
@@ -89,8 +159,8 @@ public class FindBooksActivity extends AppCompatActivity {
                     if (x.bookInfo.contains(newText))
                         results.add(x);
                 }
-
-                ((MyAdapter) myList.getAdapter()).update(results);
+                if (results != null && ((MyAdapter) myList.getAdapter()!=null))
+                    ((MyAdapter) myList.getAdapter()).update(results);
                 return false;
             }
         });
--- database/main.js
+++ database/main.js
@@ -2,7 +2,17 @@
 // pentru a porni serverel :"node main.js"
 var myDB = require("./dummydb")
 // exemplu de acces in bd
-console.log(myDB.users[1])
+
+function search_book(title,db){
+  var data =[];
+  for (let i of db){
+    if ( i.titlu.toLowerCase().search(title.toLowerCase()) != -1 || i.autor.toLowerCase().search(title.toLowerCase()) != -1 )
+      data.push(i);
+  }
+  return data;
+}
+console.log(search_book("Ion",myDB));
+
 var express        =        require("express");
 var bodyParser     =        require("body-parser");
 var app            =        express();
@@ -19,6 +29,14 @@ app.post('/login',function(req,res){
   var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
+});
+app.post("/get_books",function(req,res){
+  // Dat fiind ca rezultatul e trimis ca obiect arrayJSON,trebuie sa apelam req.body[0].ceva;
+  // console.log(req,req.body[0],req.body.title,req.body[0].title);
+  var title = req.body[0].title;
+  var x = search_book(title,myDB);
+  console.log(x);
+  res.send(x);
 });
 app.listen(3000,function(){
   console.log("Started on PORT 3000");
