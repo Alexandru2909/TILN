104b3f5a14d03081796103af5de3865dcbb8cab1
Andrei-George Caloian <48419225+caloianandreig@users.noreply.github.com>
2020-04-08 11:30:10 +0300

    Update geoloc.py

--- geo/geoloc.py
+++ geo/geoloc.py
@@ -1,5 +1,7 @@
 from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 import json
+import folium
 
 geolocator = Nominatim(user_agent="specify_your_app_name_here")
 location = geolocator.geocode("Teatrul National Vasile Alecsandri Iasi ")
@@ -13,3 +15,12 @@ for i in f:
 print(data)
 with open("locatii_output.txt", 'w') as outfile:
     json.dump(data, outfile)
+   
+geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+map1 = folium.Map(
+    location=[47.1585,27.6014],
+    tiles='cartodbpositron',
+    zoom_start=12,
+)
+dt.apply(lambda row:folium.CircleMarker(location=[row["latitude"], row["longitude"]]).add_to(map1), axis=1)
+map1
